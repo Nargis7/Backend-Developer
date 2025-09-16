a5d2b065f4a17a3ecf0990a078e5dee8ec1fc549
nargis7 <ronaldonargis786@gmail.com>
2025-09-16 18:57:42 +0530

    connected mongodb

---
+++ backend_03/src/index.js
@@ -0,0 +1,37 @@
+// require('dotenv').config({path: './.env'});
+import dotenv from 'dotenv';
+dotenv.config({ 
+  path: './.env'
+ });
+
+import connectDB from './db/index.js';
+
+
+connectDB();
+
+
+
+
+
+/*
+import express from 'express';
+const app = express();
+
+( async () => {
+  try{
+    await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`);
+    app.on("error", (error) => {
+      console.log('Error:', err);
+      throw error;
+    });
+
+    app.listen(process.env.PORT,() =>{
+      console.log(`Server is running on port ${process.env.PORT}`);
+    })
+    // console.log('Database connected successfully');
+  } catch(error){
+    console.log('Error:', error);
+    throw error;
+  }
+})()
+  */
